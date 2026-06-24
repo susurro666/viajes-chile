569ea3bf009d6e3c2e0325872d96d573fc9f39a3
Valeska Cornejo <alejandrocornejodroguett@gmail.com>
2026-06-24 00:57:40 -0300

    añadidos tooltips en redes sociales, aparte de agregar la sección de contacto

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+      window.location.hash = hash;
+      });
+    }
+  });
+});
+
